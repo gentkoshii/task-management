2e01e72b77242a8418cb3d9c229b39a4d0e71705
gent koshi <gentkoshii@gmail.com>
2024-07-13 01:23:46 +0200

    Footer setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Homepage from './pages/Homepage.jsx'
+import Navbar from './assets/Navbar.jsx'
+import Footer from './assets/Footer.jsx'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Homepage />
+      <Footer />
     </div>
   )
 }
--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
-import './Footer.scss';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
-  )
-}
+      <div>
+        
+        <div>
+          <img src="" alt="logo" />
+          <h2>TaskFlow</h2>
+          <div>
+            <div>
+              <img src="" alt="icon" />
+              <img src="" alt="icon" />
+              <img src="" alt="icon" />
+            </div>
+          </div>
+          <p>(045) 123-456</p>
+          <p>123 Street, Prishtina, Kosove</p>
+        </div>
+        
+        <div>
+          <h3>Features</h3>
+          <ul>
+            <li>
+              <Link to="/features/easy-task-management">Easy Task Management</Link>
+            </li>
+            <li>
+              <Link to="/features/task-creation">Task Creation</Link>
+            </li>
+            <li>
+              <Link to="/features/collaboration">Collaboration</Link>
+            </li>
+          </ul>
+        </div>
+        
+        <div>
+          <h3>Company</h3>
+          <ul>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/help">Help</Link>
+            </li>
+          </ul>
+        </div>
+        
+        <div>
+          <h3>Legal</h3>
+          <ul>
+            <li>
+              <Link to="/privacy" >Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to="/terms">Terms of Service</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+  );
+};
 
-export default Footer
+export default Footer;
