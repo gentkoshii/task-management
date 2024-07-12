83ab143d778ae96fe3a27fbdb822db63e47637ac
gent koshi <gentkoshii@gmail.com>
2024-07-12 23:29:29 +0200

    Login and SignUp setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import  './Login.scss'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Login = () => {
+function Login() {
+  // State variables for username and password
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+
+  const handleSubmit = async (e) => {
+    
+  };
+
   return (
-    <div className='login'>Login</div>
-  )
+    <div className="login">
+      <div className="loginSetup">
+        <form onSubmit={handleSubmit}>
+          <h2><img src="" alt="icon" />TaskFlow</h2>
+          <h3>Log in to continue</h3>
+          <input
+            type="text"
+            placeholder="example@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="************"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="submit" id="loginButton" value="Log In" />
+          <h3>Don't Have An Account ?</h3>
+          <Link to="/signup">Sign Up</Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import './SignUp.scss';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const SignUp = () => {
+function SignUp() {
+  // State variables for username and password
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+
+  const handleSubmit = async (e) => {
+    
+  };
+
   return (
-    <div className='sign-up'>SignUp</div>
-  )
+    <div className="signup">
+      <div className="signupSetup">
+        <form onSubmit={handleSubmit}>
+          <h2><img src="" alt="icon" />TaskFlow</h2>
+          <h3>Sign Up To Continue</h3>
+          <input
+            type="text"
+            placeholder="example@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="************"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input type="submit" id="loginButton" value="Sign Up" />
+          <h3>Already Have An Account ?</h3>
+          <Link to="/login">Log In</Link>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
