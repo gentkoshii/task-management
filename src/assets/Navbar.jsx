7cdfc05bd364b4da9b5b21ba33e0d11a5d1305c3
gent koshi <gentkoshii@gmail.com>
2024-07-13 01:36:19 +0200

    Navbar setup

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import './Navbar.scss';
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+      <div>
+        
+        <div>
+          <Link to="/">TaskFlow</Link>
+        </div>
+        
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/features">Features</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="Search"/>
+          </div>
+          <Link to="/login">Login</Link>
+        </div>
+        
+      </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
